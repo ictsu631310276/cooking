1992420567fa23b8ae22dda59e4904b8fca15215
ictsu631310090 <chanhom_p2@silpakorn.edu>
2023-07-09 23:57:54 +0700

    addTableDemo

---
+++ CookingGame/Assets/Script/PlayerScript/InteractionPlayerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPlayerScript : MonoBehaviour
+{
+    public static List<int> priorityInteraction = new List<int>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
