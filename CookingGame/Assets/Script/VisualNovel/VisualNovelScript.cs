542f192a979e535e7584cccb77a41926b0f31014
ictsu631310090 <chanhom_p2@silpakorn.edu>
2023-07-21 19:41:09 +0700

    ระบบจีบสาว

--- CookingGame/Assets/Script/VisualNovel/VisualNovelScript.cs
+++ CookingGame/Assets/Script/VisualNovel/VisualNovelScript.cs
@@ -2,24 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class VisualNovelScript : MonoBehaviour
 {
-    public VisualNovelData data;
+    public Sprite[] imageCharacter;
+    public Dialoge[] dataText;
     public GameObject buttomNext;
     public int nowDialogue = 0;
+    public bool pressButtom = false;
+    public bool taking = false;
+    public TextMeshProUGUI textShowSpeed;
+
+    public GameObject historyUI;
     public void NextDialogue()
     {
-        nowDialogue++;
+        if (nowDialogue == dataText.Length - 1)
+        {
+            Debug.Log("End");
+        }
+        else if (!taking)
+        {
+            nowDialogue++;
+            pressButtom = true;
+        }
+        else if (taking)
+        {
+            pressButtom = true;
+        }
     }
-    void Start()
+    public void SetSpeed()
     {
-        //buttomNext.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        if (dialogueBoxScript.talkTextSpeed == 0.05f)
+        {
+            dialogueBoxScript.talkTextSpeed = 0.01f;
+            textShowSpeed.text = "X" + 5;
+        }
+        else
+        {
+            dialogueBoxScript.talkTextSpeed = 0.05f;
+            textShowSpeed.text = "X" + 1;
+        }
         
     }
+    public void OpenHistoryButtom()
+    {
+        historyUI.SetActive(true);
+    }
+    public void CloseHistoryButtom()
+    {
+        historyUI.SetActive(false);
+    }
+    private void Start()
+    {
+        //buttomNext.SetActive(false);
+        historyUI.SetActive(false);
+    }
+    private void Update()
+    {
+
+    }
+    [System.Serializable]
+    public class Dialoge
+    {
+        public int positionSit;//1-3 4
+        public bool openImage;
+        public int imageCharacter;//ถ้าไม่ได้เปลี่ยน ไม่ต้องเปลี่ยน
+        public string name;//ถ้าไม่ได้เปลี่ยน ไม่ต้องเปลี่ยน
+        public string text;
+    }
+
 }
---
+++ CookingGame/Assets/Script/VisualNovel/dialogueBoxScript.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class dialogueBoxScript : MonoBehaviour
+{
+    public int position;
+    public VisualNovelScript data;
+    public SpriteRenderer imageCharacter;
+    public SpriteRenderer boxText;
+    public TextMeshPro nameCharacter;
+    public TextMeshPro dialogue;
+    private int numOfCharacters;
+    public static float talkTextSpeed = 0.05f;
+    private int numOfWords = 0;
+
+    public bool willFade = false;//คุมการจาง
+    private float fadeValue = 0;
+    private bool fading = false;//กำลังจางอยู่หรือไม่
+
+    IEnumerator RunText(float speed)
+    {
+        numOfCharacters = dialogue.textInfo.characterCount;
+        for (numOfWords = 0; numOfWords < numOfCharacters; numOfWords++)
+        {
+            dialogue.maxVisibleCharacters = numOfWords + 1;
+            yield return new WaitForSeconds(speed);
+        }
+        data.taking = false;
+        numOfWords = 0;
+    }
+    private void UpdateText(int now)
+    {
+        numOfCharacters = dialogue.textInfo.characterCount;
+        int i = (now > 0) ? now : now * -1;//เงื่อนไข ? จริง : เท็จ
+        if (data.dataText[i].imageCharacter == 0)
+        {
+            imageCharacter.sprite = data.imageCharacter[data.dataText[i - 1].imageCharacter];
+        }
+        else
+        {
+            imageCharacter.sprite = data.imageCharacter[data.dataText[i].imageCharacter];
+        }
+        if (data.dataText[i].name == "")
+        {
+            nameCharacter.text = data.dataText[i - 1].name;
+        }
+        else
+        {
+            nameCharacter.text = data.dataText[i].name;
+        }
+        dialogue.text = data.dataText[i].text;
+    }
+    private void Start()
+    {
+        imageCharacter.color = new Color(255,255,255, fadeValue);
+        boxText.color = new Color(255, 255, 255, fadeValue);
+        nameCharacter.color = new Color(0, 0, 0, fadeValue);
+        dialogue.color = new Color(0, 0, 0, fadeValue);
+    }
+    private void Update()
+    {        
+        if (data.pressButtom && fading && !data.taking && position == data.dataText[data.nowDialogue].positionSit)
+        {
+            data.pressButtom = false;
+            StartCoroutine(RunText(talkTextSpeed));
+        }
+        else if (data.pressButtom && fading && data.taking)
+        {
+            StopCoroutine(RunText(talkTextSpeed));
+            numOfWords += 1000;
+            dialogue.maxVisibleCharacters += 1000;
+            data.pressButtom = false;
+        }//บทดพูด
+
+        if (willFade)
+        {
+            if (fadeValue <= 255)
+            {
+                fadeValue += Time.deltaTime;
+                fading = true;
+            }
+            else
+            {
+                fading = false;
+            }
+        }
+        else if (!willFade) //จาง
+        {
+            if (fadeValue >= 0)
+            {
+                fadeValue -= Time.deltaTime;
+                fading = true;
+            }
+            else
+            {
+                fading = false;
+            }
+        }
+        imageCharacter.color = new Color(255, 255, 255, fadeValue);//จาง
+        boxText.color = new Color(255, 255, 255, fadeValue);
+        nameCharacter.color = new Color(0, 0, 0, fadeValue);
+        dialogue.color = new Color(0, 0, 0, fadeValue);
+
+        if (position == data.dataText[data.nowDialogue].positionSit)
+        {
+            willFade = data.dataText[data.nowDialogue].openImage;
+            UpdateText(data.nowDialogue);//อับเดดข้อมูล
+        }
+        Debug.Log(willFade);
+    }
+}
